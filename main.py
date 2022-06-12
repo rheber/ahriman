a669d3c395dc7a53a5834006eb513c904021d73e
rheber <rheber@rediffmail.com>
2022-06-12 16:45:32 +1000

    transformers

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ Much of this is based on https://kapernikov.com/tutorial-image-classification-wi
 """
 
 import joblib
-from skimage.color import rgba2rgb
+from skimage.color import rgba2rgb, rgb2gray
+from skimage.feature import hog
 from skimage.io import imread as imgRead
 from skimage.transform import resize as imgResize
-from sklearn.model_selection import train_test_split
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, Normalizer
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -86,6 +89,53 @@ def displayExamples(data):
         ax.set_title(label)
     plt.show()
 
+class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
+    """
+    Convert an array of RGB images to grayscale
+    """
+ 
+    def __init__(self):
+        pass
+ 
+    def fit(self, X, y=None):
+        """returns itself"""
+        return self
+ 
+    def transform(self, X, y=None):
+        """perform the transformation and return an array"""
+        return np.array([rgb2gray(img) for img in X])
+
+class HogTransformer(BaseEstimator, TransformerMixin):
+    """
+    Expects an array of 2d arrays (1 channel images)
+    Calculates hog features for each img
+    """
+ 
+    def __init__(self, y=None, orientations=9,
+                 pixels_per_cell=(8, 8),
+                 cells_per_block=(3, 3), block_norm='L2-Hys'):
+        self.y = y
+        self.orientations = orientations
+        self.pixels_per_cell = pixels_per_cell
+        self.cells_per_block = cells_per_block
+        self.block_norm = block_norm
+ 
+    def fit(self, X, y=None):
+        return self
+ 
+    def transform(self, X, y=None):
+        def local_hog(X):
+            return hog(X,
+                       orientations=self.orientations,
+                       pixels_per_cell=self.pixels_per_cell,
+                       cells_per_block=self.cells_per_block,
+                       block_norm=self.block_norm)
+ 
+        try: # parallel
+            return np.array([local_hog(img) for img in X])
+        except:
+            return np.array([local_hog(img) for img in X])
+
 def split(data):
     """Split the given data into training and test data."""
 
@@ -100,15 +150,34 @@ def split(data):
     )
     return (X_train, X_test, y_train, y_test)
 
+grayify = RGB2GrayTransformer()
+hogify = HogTransformer(
+    pixels_per_cell=(14, 14), 
+    cells_per_block=(2,2), 
+    orientations=9, 
+    block_norm='L2-Hys'
+)
+scalify = StandardScaler()
+
 def train(X_train, y_train):
     """Train a classifier on the given training data."""
+
+    X_train_gray = grayify.fit_transform(X_train)
+    X_train_hog = hogify.fit_transform(X_train_gray)
+    X_train_prepared = scalify.fit_transform(X_train_hog)
+
     classifier = SGDClassifier(random_state=42, max_iter=1000, tol=1e-3)
-    classifier.fit(X_train, y_train)
+    classifier.fit(X_train_prepared, y_train)
     return classifier
 
 def test(classifier, X_test, y_test):
-    """Test the given cliassifier against the given test data."""
-    y_pred = classifier.predict(X_test)
+    """Test the given classifier against the given test data."""
+
+    X_test_gray = grayify.transform(X_test)
+    X_test_hog = hogify.transform(X_test_gray)
+    X_test_prepared = scalify.transform(X_test_hog)
+
+    y_pred = classifier.predict(X_test_prepared)
     print(np.array(y_pred == y_test)[:25])
     print('Percentage correct: ', 100*np.sum(y_pred == y_test)/len(y_test))
 
